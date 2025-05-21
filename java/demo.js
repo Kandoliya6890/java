ec9b25702b271f0b4b48db2f7ec2c49a8981f0f0
Kandoliya6890 <mihirkandoliya6890@gmail.com>
2025-05-21 06:34:23 +0000

    done

--- java/demo.js
+++ java/demo.js
@@ -1,5 +1,15 @@
 //console.log("Hello, World!");
-var m= 1
+//var m= 1
 console.log(m);
 //console.log(m.length);
 //console.log(typeof m);
+
+/*var m=3;
+var y=++m;
+console.log(m);
+console.log(y);*/
+var m=3;
+m++;
+var y=m;
+console.log(m);
+console.log(y);
